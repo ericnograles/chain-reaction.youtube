7e4c6f93dc6296ffc8fff20d801db10bdac0e20e
Eric Nograles <ENograles@352inc.com>
2016-04-14 07:19:33 -0400

    Upgraded to React 15.  Also fixed key bug in MemeGrid template

--- src/components/MemeGrid/template.js
+++ src/components/MemeGrid/template.js
@@ -3,10 +3,10 @@ import React from 'react';
 export function MemeGridTemplate(component) {
   var memes = component.state.memes.map(memeUrl => {
     return (
-      <div className="demo-container mdl-grid">
+      <div key={memeUrl} className="demo-container mdl-grid">
         <div className="mdl-cell mdl-cell--2-col mdl-cell--hide-tablet mdl-cell--hide-phone"></div>
         <div className="demo-content mdl-color--white mdl-shadow--4dp content mdl-color-text--grey-800 mdl-cell mdl-cell--8-col">
-          <div key={memeUrl} className="meme-image-container">
+          <div className="meme-image-container">
             <img src={memeUrl} />
           </div>
         </div>
